8e4a8c55d57405d81fc63f2d5805d20f0b5c703f
yasithWimukthi <it19966922@my.sliit.lk>
2021-04-28 16:58:21 +0530

    create place model class

--- src/app/places/places.service.ts
+++ src/app/places/places.service.ts
@@ -1,13 +1,15 @@
 import { Injectable } from '@angular/core';
+import {Place} from "./place.model";
 
 @Injectable({
   providedIn: 'root'
 })
 export class PlacesService {
 
-  private _places = [];
+  private _places:Place[] = [];
 
   get places(){
+    // @ts-ignore
     return [...this._places];
   }
 
